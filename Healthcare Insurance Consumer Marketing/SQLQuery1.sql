16a900e5cb09e8bf71360a846caba5b8d50a1c6a
Haoming (Cody) Chen <107047989+HaomingChen1998@users.noreply.github.com>
2023-02-16 12:08:22 -0500

    Add files via upload

---
+++ Healthcare Insurance Consumer Marketing/SQLQuery1.sql
@@ -0,0 +1,43 @@
+-- The age range for this dataset is 18-64
+Select distinct age
+FROM expense
+ORDER BY age
+
+-- The charge per person for middle age group = $15,193
+-- The charge per person for adult group = $9,397
+SELECT
+avg(charges) as charge_per_person,
+(CASE
+WHEN age >= 18 AND age <=30 THEN 'adult'
+WHEN age >30 AND age <=64 THEN 'middle_age'
+END) as age_group
+FROM expense
+GROUP BY 
+(CASE WHEN age >= 18 AND age <=30 THEN 'adult'
+WHEN age >30 AND age <=64 THEN 'middle_age'
+END)
+
+-- The charge per person for smoker = $32,050
+-- The charge per person for non-smoker = $8,434
+
+SELECT
+avg(charges) as charge_per_person,
+smoker
+FROM expense
+GROUP BY smoker
+
+
+-- The total amount of the company's adult customers = 444
+-- The total amount of the company's middle age customers = 894
+select 
+SUM(CASE WHEN age >= 18 AND age <=30 THEN 1 ELSE 0 END) as total_adult_customer,
+SUM(CASE WHEN age >30 AND age <=64 THEN 1 ELSE 0 END) as total_middle_age_customer
+FROM expense
+
+-- The total amount of the company's smoking customers = 274
+-- The total amount of the company's non-smoking customers = 1064
+
+select 
+SUM(CASE WHEN smoker = 'yes' THEN 1 ELSE 0 END) as total_smoking_customer,
+SUM(CASE WHEN smoker = 'no' THEN 1 ELSE 0 END) as total_non_smoking_customer
+FROM expense
