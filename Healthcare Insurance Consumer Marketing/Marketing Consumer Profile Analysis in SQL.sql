23217202c37830210c0238cd9dfc7d92db71ccc4
Haoming (Cody) Chen <107047989+HaomingChen1998@users.noreply.github.com>
2023-02-18 13:19:46 -0500

    Update Marketing Consumer Profile Analysis in SQL.sql

--- Healthcare Insurance Consumer Marketing/Marketing Consumer Profile Analysis in SQL.sql
+++ Healthcare Insurance Consumer Marketing/Marketing Consumer Profile Analysis in SQL.sql
@@ -3,10 +3,10 @@ Select distinct age
 FROM expense
 ORDER BY age
 
--- The charge per person for middle age group = $15,193
--- The charge per person for adult group = $9,397
+-- The revenue per customer for middle age group = $15,193
+-- The revenue per customer for adult group = $9,397
 SELECT
-avg(charges) as charge_per_person,
+avg(charges) as revenue per customer,
 (CASE
 WHEN age >= 18 AND age <=30 THEN 'adult'
 WHEN age >30 AND age <=64 THEN 'middle_age'
@@ -17,7 +17,7 @@ GROUP BY
 WHEN age >30 AND age <=64 THEN 'middle_age'
 END)
 
--- The charge per person for middle age group is about 62% higher than adult age group.
+-- The revenue per customer for middle age group is about 62% higher than adult age group.
 SELECT
 (AVG(CASE WHEN age >30 AND age <=64 THEN charges END)
 /
@@ -25,30 +25,30 @@ AVG(CASE WHEN age >= 18 AND age <=30 THEN charges END)-1)*100
 FROM expense
 
 
--- The charge per person for smoker = $32,050
--- The charge per person for non-smoker = $8,434
+-- The revenue per customer for smoker = $32,050
+-- The revenue per customer for non-smoker = $8,434
 SELECT
-avg(charges) as charge_per_person,
+avg(charges) as revenue per customer,
 smoker
 FROM expense
 GROUP BY smoker
 
--- The charge per person for smoker is about 280% higher than non-smoker.
+-- The revenue per customer for smoker is about 280% higher than non-smoker.
 SELECT
 (AVG(CASE WHEN smoker = 'yes' THEN charges END)
 /
 AVG(CASE WHEN smoker = 'no' THEN charges END)-1)*100
 FROM expense
 
--- The total amount of the company's adult customers = 444
--- The total amount of the company's middle age customers = 894
+-- The total number of the company's adult customers = 444
+-- The total number of the company's middle age customers = 894
 select 
 SUM(CASE WHEN age >= 18 AND age <=30 THEN 1 ELSE 0 END) as total_adult_customer,
 SUM(CASE WHEN age >30 AND age <=64 THEN 1 ELSE 0 END) as total_middle_age_customer
 FROM expense
 
--- The total amount of the company's smoking customers = 274
--- The total amount of the company's non-smoking customers = 1064
+-- The total number of the company's smoking customers = 274
+-- The total number of the company's non-smoking customers = 1064
 select 
 SUM(CASE WHEN smoker = 'yes' THEN 1 ELSE 0 END) as total_smoking_customer,
 SUM(CASE WHEN smoker = 'no' THEN 1 ELSE 0 END) as total_non_smoking_customer
